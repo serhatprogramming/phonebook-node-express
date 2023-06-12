58cfa3b23f4166db3508a628e03a8a9908d9dd82
serhatprogramming <serhatprogramming@gmail.com>
2023-06-12 14:45:24 -0400

    morgan tokens added

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
+morgan.token("body", function (req, res) {
+  return JSON.stringify(req.body);
+});
 
+morgan.token("code", function (req, res) {
+  return res.statusCode;
+});
+
+app.use(morgan(":method :url :code - :response-time ms :body "));
 let persons = [
   {
     id: 1,
